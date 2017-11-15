cbbd501e0703abff17c07b6e9d0978cdfa6e58e5
Moon_Slight <moon-slight@users.noreply.github.com>
2017-11-15 16:34:38 +0800

    add change weapon function

--- Shoot Balloon/Assets/Script/Weapon.cs
+++ Shoot Balloon/Assets/Script/Weapon.cs
@@ -6,7 +6,7 @@ public class Weapon : MonoBehaviour {
 
 	public GameObject Front;
 	public GameObject Back;
-	public GameObject BulletPrefab;
+	public GameObject[] weapons;
 	public Transform BulletSpawn;
 	public int BulletNum;
 	public int score;
@@ -17,6 +17,7 @@ public class Weapon : MonoBehaviour {
 	void Start () {
 		BulletNum = 10;
 		score = 0;
+		weapons = new GameObject[6];
 	}
 	
 	// Update is called once per frame
@@ -28,7 +29,7 @@ public class Weapon : MonoBehaviour {
 			}
 
 			//fire
-			if (Input.GetMouseButtonDown(0) && BulletNum >= 1) {
+			if (Input.GetMouseButtonDown(0)) {
 
 				RaycastHit Hit;
 				shootDirection = Front.transform.position - Back.transform.position;
@@ -50,7 +51,48 @@ public class Weapon : MonoBehaviour {
 
 			//choose weapon
 			if (Input.GetKey (KeyCode.Space)) {
+				
+				RaycastHit Hit;
 				shootDirection = Front.transform.position - Back.transform.position;
+				Ray ShootRay = new Ray (BulletSpawn.position, Vector3.Normalize(shootDirection));
+
+				if (Physics.Raycast (ShootRay, out Hit, 300f)) {
+					if (Hit.collider.name == "Ak-47" || Hit.collider.name == "Ak-47 (1)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [0].SetActive (true);
+					}
+
+					else if (Hit.collider.name == "PM-40_Variant2" || Hit.collider.name == "PM-40_Variant2 (1)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [1].SetActive (true);
+					}
+
+					else if (Hit.collider.name == "PM-40_Variant4" || Hit.collider.name == "PM-40_Variant4 (1)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [2].SetActive (true);
+					}
+
+					else if (Hit.collider.name == "M4A1 Sopmod" || Hit.collider.name == "M4A1 Sopmod (1)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [3].SetActive (true);
+					}
+
+					else if (Hit.collider.name == "Skorpion VZ" || Hit.collider.name == "Skorpion VZ (1)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [4].SetActive (true);
+					}
+
+					else if (Hit.collider.name == "UMP-45" || Hit.collider.name == "UMP-45 (1)" || Hit.collider.name == "UMP-45 (2)") {
+						for (int i = 0; i < 6; i++)
+							weapons [i].SetActive (false); 
+						weapons [5].SetActive (true);
+					}
+				}
 			}
 		}
 
